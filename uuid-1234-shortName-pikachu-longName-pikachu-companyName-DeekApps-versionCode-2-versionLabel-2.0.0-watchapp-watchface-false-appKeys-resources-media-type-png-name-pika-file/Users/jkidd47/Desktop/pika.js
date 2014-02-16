41d74898c99f32d71a8d02b1045e4963513f1b30
jkidd93 <patelh4@tcnj.edu>
2014-02-15 23:35:56 -0500

    Create pika.js

---
+++ uuid-1234-shortName-pikachu-longName-pikachu-companyName-DeekApps-versionCode-2-versionLabel-2.0.0-watchapp-watchface-false-appKeys-resources-media-type-png-name-pika-file/Users/jkidd47/Desktop/pika.js
@@ -0,0 +1,21 @@
+	{
+  "uuid": "1234",
+  "shortName": "pikachu",
+  "longName": "pikachu",
+  "companyName": "DeekApps",
+  "versionCode": 2,
+  "versionLabel": "2.0.0",
+  "watchapp": {
+    "watchface": false
+  },
+  "appKeys": {},
+  "resources": {
+    "media": [
+      {
+        "type": "png",
+        "name": "pika",
+        "file": "/Users/jkidd47/Desktop/pika.png"
+      }
+    ]
+  }
+}
